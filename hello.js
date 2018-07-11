def477add14fe9b791b96c4a6c87331b4e1dad66
mgriffin78 <mgriffin78@hotmail.com>
2018-07-11 14:42:32 -0700

    v2

--- hello.js
+++ hello.js
@@ -3,4 +3,4 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Hello World version 1!!!');
-}).listen(8000)i;
+}).listen(8000);
--- init.py
+++ init.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 import os
-cmd = 'node hello.js'
+cmd = 'node /app/hello.js'
 os.system(cmd)
